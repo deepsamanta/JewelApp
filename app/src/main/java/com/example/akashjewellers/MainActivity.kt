72de685403573fc5cfca5b883806b49f826d8026
Deep <deepsamanta001@gmail.com>
2025-04-19 20:18:38 +0000

    first app changes

--- app/src/main/java/com/example/akashjewellers/MainActivity.kt
+++ app/src/main/java/com/example/akashjewellers/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.akashjewellers
 
+import androidx.compose.material3.Surface
+
+
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,6 +30,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -30,21 +38,25 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.akashjewellers.ui.theme.AkashJewellersTheme
 import com.example.akashjewellers.ui.theme.EmailLogin
 import com.example.akashjewellers.ui.theme.MainScreen
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,19 +65,66 @@ class MainActivity : ComponentActivity() {
         setContent {
             AkashJewellersTheme {
 
-
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "MainScreen", builder = {
-                    composable(route = "MainScreen") {
-                        MainScreen(navController)
-                    }
-                    composable(route = "EmailLoginScreen") {
-                        EmailLogin(navController)
-                    }
-                })
+                Surface(color = Color.White, modifier = Modifier.fillMaxSize()) {
+                    com.example.akashjewellers.Navigation()
+                }
             }
         }
     }
 }
 
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "splash_screen"
+    ) {
+        composable("splash_screen") {
+            SplashScreen(navController = navController)
+        }
+        composable(route = "MainScreen") {
+            MainScreen(navController)
+        }
+        composable(route = "EmailLoginScreen") {
+            EmailLogin(navController)
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
+    // Animation
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.7f,
+            // tween Animation
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                })
+        )
+        // Customize the delay time
+        delay(1000L)
+        navController.navigate("MainScreen")
+    }
+
+    // Image
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Change the logo
+        Image(
+            painter = painterResource(id = R.drawable.splashscreen),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(scale.value)
+        )
+    }
+}
 
